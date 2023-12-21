5d52a963acc0dcd95512688d8483f434958412ef
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2023-12-21 16:09:56 +0100

    .

--- src/test/script/allTests.py
+++ src/test/script/allTests.py
@@ -6,11 +6,12 @@ os.chdir("../../../")
 print("=========== printString.deca ===========")
 os.system("./src/main/bin/decac ./src/test/deca/syntax/valid/printString.deca")
 out = subprocess.check_output("./global/bin/ima ./src/test/deca/syntax/valid/printString.ass", shell=True)
-assert (out == b'Hello World ! Second Argument\n'
-               b'Second Println\n'
-               b'Print Normal 1, Print Normal 2\n')
+assert (out == b"Hello World ! Second Argument\n"
+               b"Second Println\n"
+               b"Print Normal 1, Print Normal 2\n")
 
 print("=========== printIntFloat.deca ===========")
 os.system("./src/main/bin/decac ./src/test/deca/syntax/valid/printIntFloat.deca")
 out = subprocess.check_output("./global/bin/ima ./src/test/deca/syntax/valid/printIntFloat.ass", shell=True)
-print(out)
+assert (out == b"Chaine de Int : 1 2 42 -1 0 -42\n"
+               b"Chaine de Float : 1.22000e+00 -4.24242e+01 0.00000e+003.1416 -2.78000e+00\n")
