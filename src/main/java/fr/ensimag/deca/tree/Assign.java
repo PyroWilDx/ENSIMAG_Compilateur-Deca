4d6ab3fa3b98e4a39b03db53780898d534ec3659
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2023-12-28 19:14:38 +0100

    .

--- src/main/java/fr/ensimag/deca/tree/Assign.java
+++ src/main/java/fr/ensimag/deca/tree/Assign.java
@@ -34,6 +34,7 @@ public class Assign extends AbstractBinaryExpr {
             ClassDefinition currentClass) throws ContextualError {
         Type type = this.getLeftOperand().verifyLValue(compiler, localEnv, currentClass);
         this.getRightOperand().verifyRValue(compiler, localEnv, currentClass, type);
+        this.setType(type);
         return type;
     }
 
