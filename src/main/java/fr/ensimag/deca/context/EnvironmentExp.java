64b4354325fc21a1c3ec04bb0c7c62c80e31964e
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2024-01-10 10:40:59 +0100

    .

--- src/main/java/fr/ensimag/deca/context/EnvironmentExp.java
+++ src/main/java/fr/ensimag/deca/context/EnvironmentExp.java
@@ -8,18 +8,18 @@ import java.util.Set;
 
 /**
  * Dictionary associating identifier's ExpDefinition to their names.
- * 
+ * <p>
  * This is actually a linked list of dictionaries: each EnvironmentExp has a
  * pointer to a parentEnvironment, corresponding to superblock (eg superclass).
- * 
- * The dictionary at the head of this list thus corresponds to the "current" 
+ * <p>
+ * The dictionary at the head of this list thus corresponds to the "current"
  * block (eg class).
- * 
- * Searching a definition (through method get) is done in the "current" 
- * dictionary and in the parentEnvironment if it fails. 
- * 
+ * <p>
+ * Searching a definition (through method get) is done in the "current"
+ * dictionary and in the parentEnvironment if it fails.
+ * <p>
  * Insertion (through method declare) is always done in the "current" dictionary.
- * 
+ *
  * @author gl47
  * @date 01/01/2024
  */
@@ -29,11 +29,12 @@ public class EnvironmentExp {
     // d'empilement).
     private EnvironmentExp parentEnvironment;
     private HashMap<Symbol, ExpDefinition> env;
-    
+
     public EnvironmentExp(EnvironmentExp parentEnvironment) {
         this.parentEnvironment = parentEnvironment;
         this.env = new HashMap<>();
     }
+
     public Symbol disjointUnion(EnvironmentExp envExp) {
         // if the union isn't disjoint, returns the symbol occuring twice
         for (Symbol s1 : envExp.getKeys()) {
@@ -60,12 +61,11 @@ public class EnvironmentExp {
     }
 
 
-
     public static class DoubleDefException extends Exception {
         private static final long serialVersionUID = -2733379901827316441L;
     }
 
-    public static EnvironmentExp empile(EnvironmentExp env1, EnvironmentExp env2)  {
+    public static EnvironmentExp empile(EnvironmentExp env1, EnvironmentExp env2) {
         EnvironmentExp copyEnv1 = env1.copy();
         EnvironmentExp copyEnv2 = env2.copy();
         EnvironmentExp dernier = copyEnv1;
@@ -89,18 +89,14 @@ public class EnvironmentExp {
 
     /**
      * Add the definition def associated to the symbol name in the environment.
-     * 
+     * <p>
      * Adding a symbol which is already defined in the environment,
      * - throws DoubleDefException if the symbol is in the "current" dictionary
      * - or, hides the previous declaration otherwise.
-     * 
-     * @param name
-     *            Name of the symbol to define
-     * @param def
-     *            Definition of the symbol
-     * @throws DoubleDefException
-     *             if the symbol is already defined at the "current" dictionary
      *
+     * @param name Name of the symbol to define
+     * @param def  Definition of the symbol
+     * @throws DoubleDefException if the symbol is already defined at the "current" dictionary
      */
     public void declare(Symbol name, ExpDefinition def) throws DoubleDefException {
         if (env.containsKey(name)) {
@@ -109,11 +105,13 @@ public class EnvironmentExp {
         env.put(name, def);
         // Done
     }
+
     public EnvironmentExp copy() {
         EnvironmentExp copy;
         if (this.parentEnvironment == null) copy = new EnvironmentExp(null);
         else copy = new EnvironmentExp(this.parentEnvironment.copy());
-        copy.setEnv((HashMap<Symbol, ExpDefinition>) this.env.clone());//TODO pas de problème
+        HashMap<Symbol, ExpDefinition> clone = new HashMap<>(this.env);
+        copy.setEnv(clone);
         return copy;
     }
 
