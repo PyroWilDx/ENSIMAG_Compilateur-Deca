2e49fefcfd1f7f9ef5b05a19ba0b1514758487ec
antoine <abnotrogiente@gmail.com>
2024-01-11 19:08:37 +0100

    .

--- src/test/script/tests_invalid.sh
+++ src/test/script/tests_invalid.sh
@@ -2,5 +2,10 @@
 
 for fichier in ../deca/context/invalid/langage_sans_objet/*/*.deca
 do
-  ./launchers/test_context $fichier
+  ./launchers/test_context $fichier  1>debug 2>error;
+  sed -r 's/.*regle.*:([0-9]*):([0-9]*):/Ligne \1:/g' error;
+  echo =============== REPONSE ATTENDUE ===============;
+  sed -n '5p' < $fichier | sed 's/\/\/\ *//g';
+  echo;
+  echo;
 done
