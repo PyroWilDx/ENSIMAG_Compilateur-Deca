614c5b4d7238558f997cefe6e1db899605096f66
antoine <abnotrogiente@gmail.com>
2024-01-12 18:09:39 +0100

    .

--- src/test/script/test_failed_number.sh
+++ src/test/script/test_failed_number.sh
@@ -1,1 +1,6 @@
-#! bin/sh
+#! /bin/sh
+
+n=$(expr $1 + 1);
+file=$(sed -n "$n"p failed_files);
+echo $file;
+./launchers/test_context $file;
--- src/test/script/tests_invalid.sh
+++ src/test/script/tests_invalid.sh
@@ -1,29 +1,66 @@
 #! /bin/sh
 
+failed_files=;
+
+echo "" > failed_files;
+
+RED='\033[0;31m';
+GREEN='\033[0;32m';
+NC='\033[0m'; # No Color
+
+compteur=0;
+valids=0
+
+
+isItTheGoodError () {
+  compteur=$(expr $compteur + 1);
+  if test "$real_error" = "$expected_error"
+    then
+      echo "${GREEN}PASSED${NC}";
+      valids=$(expr $valids + 1);
+    else
+      echo "${RED}FAILED${NC}";
+      failed_files="$failed_files $fichier";
+    fi
+}
+
+testChemin() {
+  for fichier in $chemin
+  do
+    basename "$fichier" | sed 's/\.deca//g' | sed 's/_/\ /' | sed 's/_/\./';
+    ./launchers/test_context "$fichier"  1>debug 2>error;
+    real_error=$(sed -r 's/.*regle.*:([0-9]*):([0-9]*):/Ligne \1:/g' error);
+    echo $real_error;
+    echo =============== REPONSE ATTENDUE ===============;
+    expected_error=$(sed -n '5p' < "$fichier" | sed 's/\/\/\ *//g');
+    echo $expected_error;
+    isItTheGoodError;
+    echo;
+    echo;
+  done
+}
+
 echo ==========================================================;
 echo "                   LANGUAGE SANS OBJET"
 echo ==========================================================;
-for fichier in ../deca/context/invalid/langage_sans_objet/*/*.deca
-do
-  basename "$fichier" | sed 's/\.deca//g' | sed 's/_/\ /' | sed 's/_/\./';
-  ./launchers/test_context "$fichier"  1>debug 2>error;
-  sed -r 's/.*regle.*:([0-9]*):([0-9]*):/Ligne \1:/g' error;
-  echo =============== REPONSE ATTENDUE ===============;
-  sed -n '5p' < "$fichier" | sed 's/\/\/\ *//g';
-  echo;
-  echo;
-done
+chemin="../deca/context/invalid/langage_sans_objet/*/*.deca";
+testChemin;
+
 echo;echo;
 echo ==========================================================;
 echo "                   LANGUAGE SANS OBJET"
 echo ==========================================================;
-for fichier in ../deca/context/invalid/langage_essentiel/*/*.deca
+chemin="../deca/context/invalid/langage_essentiel/*/*.deca"
+testChemin;
+
+rm error debug;
+
+echo "${GREEN}PASSED : $valids / $compteur${NC}";
+echo "failed : ";
+i=1;
+for failed_file in $failed_files
 do
-  basename "$fichier" | sed 's/\.deca//g' | sed 's/_/\ /' | sed 's/_/\./';
-  ./launchers/test_context "$fichier"  1>debug 2>error;
-  sed -r 's/.*regle.*:([0-9]*):([0-9]*):/Ligne \1:/g' error;
-  echo =============== REPONSE ATTENDUE ===============;
-  sed -n '5p' < "$fichier" | sed 's/\/\/\ *//g';
-  echo;
-  echo;
+  echo "$i : ${RED}$(basename "$failed_file")${NC}";
+  i=$(expr $i + 1);
+  echo $failed_file >> failed_files;
 done
