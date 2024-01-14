d65d4b08958d7d1a790076dbac2bab172639d785
antoine <abnotrogiente@gmail.com>
2024-01-14 17:09:46 +0100

    .

--- src/test/script/tests_context_failed.sh
+++ src/test/script/tests_context_failed.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
-
-n=$(expr $1 + 1);
+# Runs text_context on a file which didn't pass test_context_invalid
+echo "indicate number of failed_file"
+echo "(index in the file or plotted by test_context_invalid.sh)"
+read n
+n=$(expr $n + 1);
 file=$(sed -n "$n"p failed_files);
 echo $file;
 ./launchers/test_context $file;
