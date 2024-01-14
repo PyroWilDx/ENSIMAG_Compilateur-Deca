68d60bfc910a4277d88649069024d92360cb9c26
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2024-01-14 16:44:51 +0100

    .

--- src/test/java/fr/ensimag/deca/context/TestPlusAdvanced.java
+++ src/test/java/fr/ensimag/deca/context/TestPlusAdvanced.java
@@ -1,15 +1,20 @@
 package fr.ensimag.deca.context;
 
+import fr.ensimag.deca.CompilerOptions;
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.tree.AbstractExpr;
-import fr.ensimag.deca.tree.ConvFloat;
-import fr.ensimag.deca.tree.Plus;
+import fr.ensimag.deca.tree.*;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
+
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 /**
  * Test for the Plus node using mockito, using @Mock and @Before annotations.
@@ -23,59 +28,61 @@ public class TestPlusAdvanced {
     final Type FLOAT = new FloatType(null);
 
     @Mock
-    AbstractExpr intexpr1;
+    AbstractExpr intexpr1 = new IntLiteral(1);
     @Mock
-    AbstractExpr intexpr2;
+    AbstractExpr intexpr2 = new IntLiteral(2);
     @Mock
-    AbstractExpr floatexpr1;
+    AbstractExpr floatexpr1 = new FloatLiteral(3);
     @Mock
-    AbstractExpr floatexpr2;
-
+    AbstractExpr floatexpr2 = new FloatLiteral(4);
     DecacCompiler compiler;
-    
+    EnvironmentExp envExp;
+
     @BeforeEach
     public void setup() throws ContextualError {
         MockitoAnnotations.initMocks(this);
-        compiler = new DecacCompiler(null, null);
-        when(intexpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
-        when(intexpr2.verifyExpr(compiler, null, null)).thenReturn(INT);
-        when(floatexpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
-        when(floatexpr2.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        compiler = new DecacCompiler(new CompilerOptions(), null);
+        envExp = new EnvironmentExp(null);
+        // TODO: Fix des erreurs de l'environnement
+        when(intexpr1.verifyExpr(compiler, envExp, null)).thenReturn(INT);
+        when(intexpr2.verifyExpr(compiler, envExp, null)).thenReturn(INT);
+        when(floatexpr1.verifyExpr(compiler, envExp, null)).thenReturn(FLOAT);
+        when(floatexpr2.verifyExpr(compiler, envExp, null)).thenReturn(FLOAT);
     }
 
     @Test
     public void testIntInt() throws ContextualError {
         Plus t = new Plus(intexpr1, intexpr2);
         // check the result
-        assertTrue(t.verifyExpr(compiler, null, null).isInt());
+        assertTrue(t.verifyExpr(compiler, envExp, null).isInt());
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(intexpr2).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, envExp, null);
+        verify(intexpr2).verifyExpr(compiler, envExp, null);
     }
 
     @Test
     public void testIntFloat() throws ContextualError {
         Plus t = new Plus(intexpr1, floatexpr1);
         // check the result
-        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        assertTrue(t.verifyExpr(compiler, envExp, null).isFloat());
         // ConvFloat should have been inserted on the right side
         assertTrue(t.getLeftOperand() instanceof ConvFloat);
         assertFalse(t.getRightOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, envExp, null);
+        verify(floatexpr1).verifyExpr(compiler, envExp, null);
     }
 
     @Test
     public void testFloatInt() throws ContextualError {
         Plus t = new Plus(floatexpr1, intexpr1);
         // check the result
-        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        assertTrue(t.verifyExpr(compiler, envExp, null).isFloat());
         // ConvFloat should have been inserted on the right side
         assertTrue(t.getRightOperand() instanceof ConvFloat);
         assertFalse(t.getLeftOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, envExp, null);
+        verify(floatexpr1).verifyExpr(compiler, envExp, null);
     }
 }
