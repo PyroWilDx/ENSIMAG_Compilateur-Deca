a9a7137523a2f6ba83ab2fb7099da81da729723f
haydara <core.editor>
2024-01-16 12:05:02 +0100

    .

---
+++ src/main/deca/Test.java
@@ -0,0 +1,10 @@
+//----------------------------------------------------
+Class Test{
+    public static void main(String[] args) {
+        EventsManager events = new EventsManager();
+        while(true){
+            event = getInput() // à définir
+            //à continuer apres une discussion avec le groupe
+        }
+    }
+        }
