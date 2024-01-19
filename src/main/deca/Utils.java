d0f94a4d2a603fdba3d3ce8421b25334d5bc4fd4
antoine <abnotrogiente@gmail.com>
2024-01-19 12:37:57 +0100

    .

--- src/main/deca/Utils.java
+++ src/main/deca/Utils.java
@@ -1,23 +1,53 @@
+class Color {
+    boolean bit1 = true;
+    boolean bit2 = true;
+    int index = 124;
+    void setBlack() {
+        this.bit1 = false;
+        this.bit2 = false;
+        this.index = 124;
+    }
+    void setDark() {
+        this.bit1 = false;
+        this.bit2 = true;
+        this.index = 125;
+    }
+    void setLight() {
+        this.bit1 = true;
+        this.bit2 = false;
+        this.index = 126;
+    }
+    void setWhite() {
+        this.bit1 = true;
+        this.bit2 = true;
+        this.index = 127;
+    }
+    boolean isWhite() {
+        return this.bit1 && this.bit2;
+    }
+    boolean isBlack() {
+        return !this.bit1 && !this.bit1;
+    }
+    int getTileIndex() {
+        return index;
+    }
+    boolean isSameColor(Color color) {
+        return this.bit1 == color.bit1 && this.bit2 = color.bit2;
+    }
 
+}
 class BackGroundMapMod {
-    protected boolean white = true;
-    protected boolean black = false;
+    protected Color color = new Color();
     protected boolean user = false;
     protected boolean changed = false;
 
-    public void setWhite() {
-        this.changed = true;
-        this.white = true;
-        this.black = false;
-        this.user = false;
+    public void setColor(Color c) {
+        if (!this.color.isSameColor(c)) {
+            this.changed = true;
+            this.color = c;
+        }
     }
 
-    public void setBlack() {
-        this.changed = true;
-        this.white = false;
-        this.black = true;
-        this.user = false;
-    }
 
     public void setUser() {
         this.changed = true;
@@ -30,19 +60,8 @@ class BackGroundMapMod {
         this.changed = changed;
     }
 
-    boolean isWhite() {
-        return white;
-    }
-
-    boolean isBlack() {
-        return black;
-    }
-
-    boolean isUser() {
-        return user;
-    }
-    boolean hasChanged() {
-        return changed;
+    Color getColor() {
+        return this.color;
     }
 }
 class DrawEvent {
@@ -118,6 +137,20 @@ class Utils {
             ld [bc], bc
             "
             ); // TODO VRAIMENT PAS SUR.
+    void setBackGroundInTileMap(int index) asm (
+        "
+        ld hl, sp + 6
+        ld a, [hl]
+        ld hl, $9800
+        ld bc, $400
+        setBackGroundInTileMapLoop:
+            ld [hli], a
+            dec bc
+            ld a, b
+            or a, c
+            jp nz, setBackGroundInTileMapLoop: ; Jump to COpyTiles, if the z flag is not set. (the last operation had a non zero result)
+        "
+    );
     void pushInTileMap(int x, int y, int tileIndex) asm(
             "
             ; a = y
