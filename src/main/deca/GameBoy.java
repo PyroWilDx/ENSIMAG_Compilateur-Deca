37f248346ef7bf40d2667a907c574ffc861a2f94
antoine <abnotrogiente@gmail.com>
2024-01-19 01:48:57 +0100

    .

--- src/main/deca/GameBoy.java
+++ src/main/deca/GameBoy.java
@@ -61,21 +61,26 @@
 class Color {
     boolean bit1 = false;
     boolean bit2 = false;
+    int index = 124;
     void setBlack() {
         this.bit1 = false;
         this.bit2 = false;
+        this.index = 124;
     }
     void setDark() {
         this.bit1 = false;
         this.bit2 = true;
+        this.index = 125;
     }
     void setLight() {
         this.bit1 = true;
         this.bit2 = false;
+        this.index = 126;
     }
     void setWhite() {
         this.bit1 = true;
         this.bit2 = true;
+        this.index = 127;
     }
     boolean isWhite() {
         return this.bit1 && this.bit2;
@@ -87,12 +92,17 @@ class Color {
 }
 
 class GameBoy {
-    protected DoubleLinkedListEvents drawEvents = new DoubleLinkedListEvents();
-    protected int width = 160;
-    protected int height = 144;
+    protected DrawEventList drawEvents = new DrawEventList();
+    protected int width = 20;
+    protected int pixelWidth = 160;
+    protected int height = 18;
+    protected int pixelHeight = 144;
     protected Utils utils = new Utils();
-    protected boolean isWhiteMapLabelDefined = false;
-
+    protected BackGroundMapMod map = new backGroundMapMod();
+    protected Color WHITE = new Color();
+    protected Color LIGHT = new Color();
+    protected Color DARK = new Color();
+    protected Color BLACK = new Color();
     int getWidth() {
         return this.width;
     }
@@ -102,7 +112,11 @@ class GameBoy {
     }
 
     void init() {
-        this.includeHardware();
+        WHITE.setWhite();
+        BLACK.setBlack();
+        DARK.setDark();
+        LIGHT.setLight();
+        //this.includeHardware();
         this.includeTextMacro();
         this.includeTextUtils();
         this.includeMemoryUtils();
@@ -113,6 +127,22 @@ class GameBoy {
     }
     void Owninit () asm (
     "
+    ; Shut down audio circuitry
+    ld a, 0
+    ld [rNR52], a
+
+    ; On met les tiles elementaires dans la mémoire
+    ld de, ElementaryTiles
+    ld hl, $97c0; Ce seront les quatres dernières tiles
+    ld bc, ElementaryTiles - ElementaryTilesEnd
+    call CopyDEintoMemoryAtHL
+
+    ; On met la map toute blanche
+    ld de, WhiteTilemap
+    ld hl, $9800
+    ld bc, WhiteTilemap - WhiteTilemapEnd
+    call CopyDEintoMemoryAtHL
+
 
     ; Les tiles élémentaire
     ElementaryTiles :
@@ -122,31 +152,6 @@ class GameBoy {
     db $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff
     ElementaryTilesEnd :
 
-    ElementaryTiles :
-    db $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00
-    db $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff
-    db $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00
-    db $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff
-    ElementaryTilesEnd :
-
-    WhiteTile :
-    db $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff, $ff,$ff
-    WhiteTileEnd :
-
-    BlackTile :
-    db $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00
-    BlackTileEnd :
-
-
-
-
-    ; On met les tiles elementaires dans la mémoire
-    jp
-    ld de, WhiteTile
-    ld hl, $9000
-    ld bc, WhiteTile - WhiteTile
-    call Memcopy
-
     ; Label menant à une map toute blanche
     WhiteTilemap:
     db $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, 0,0,0,0,0,0,0,0,0,0,0,0
@@ -167,7 +172,7 @@ class GameBoy {
     db $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, 0,0,0,0,0,0,0,0,0,0,0,0
     db $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, 0,0,0,0,0,0,0,0,0,0,0,0
     db $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, 0,0,0,0,0,0,0,0,0,0,0,0
-    WhiteTilemap:
+    WhiteTilemapEnd:
 
     ; Label menant à une map toute blanche
     BlackTilemap:
@@ -189,57 +194,63 @@ class GameBoy {
     db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, 0,0,0,0,0,0,0,0,0,0,0,0
     db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, 0,0,0,0,0,0,0,0,0,0,0,0
     db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, 0,0,0,0,0,0,0,0,0,0,0,0
-    BlackTilemap:
-
-
-    UpdateKeys:
-    ; Poll half the controller
-    ld a, P1F_GET_BTN
-    call .onenibble
-    ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
-
-            ; Poll the other half
-    ld a, P1F_GET_DPAD
-    call .onenibble
-    swap a ; A3-0 = unpressed directions; A7-4 = 1
-    xor a, b ; A = pressed buttons + directions
-    ld b, a ; B = pressed buttons + directions
-
-    ; And release the controller
-    ld a, P1F_GET_NONE
-    ldh [rP1], a
-
-    ; Combine with previous wCurKeys to make wNewKeys
-    ld a, [wCurKeys]
-    xor a, b ; A = keys that changed state
-    and a, b ; A = keys that changed to pressed
-    ld [wNewKeys], a
-    ld a, b
-    ld [wCurKeys], a
-            ret
-
+    BlackTilemapEnd:
     " // TODO commencer la gameloop
     ); // TODO pour faire les trucs assembleurs de bases genre main loop et tout jsp
 
-    void clearBackGround() asm (
+    void updateScreen() {
+        DrawEvent event = this.drawEvents.getFirst();
+        this.turnScreenOff();
+        if (this.map.hasChanged()) {
+            if (this.map.isWhite) {
+                this.copyWhiteMapIntoMemory();
+            }
+        }
+        while(event.hasNext()) {
+            this.utils.push(event.getX(), event.getY(), event.getTileIndex());
+            event = event.getNext();
+        }
+        this.turnScreenOn();
+    }
+    void turnScreenOff() asm (
         "
         call WaitForOneVBlank
-        call ClearBackground
+        ; Turn the LCD off
+        ld a, 0
+        ld [rLCDC], a
         "
     );
-    void set(int indice, int x, int y) {
-        this.clearBackGround();
-        int mapOffset = 0x9800 + (y * 32) + x;
-        this.utils.push(mapOffset, indice);
+    void turnScreenOn() asm (
+        "
+        ; Turn the LCD on
+        ld a, LCDCF_ON | LCDCF_BGON
+        ld [rLCDC], a
+        "
+    );
+    void setTile(int tileIndex, int x, int y) {
+        this.drawEvents.add(tileIndex, x, y);
     }
-    void setTileMapWhite(Color color) asm(
+    void setbackGroundWhite() {
+        if (!this.map.isWhite) {
+            this.map.setWhite();
+        }
+    }
+    void copyWhiteMapIntoMemory() asm (
         "
         ld de, WhiteTilemap
         ld hl, $9800
-        ld bc, WhiteTilemapEnd - WhiteTilemap
-        call Memcopy
+        ld bc, WhiteTilemap - WhiteTilemapEnd
+        call CopyDEintoMemoryAtHL
         "
-        );
+    );
+    void copyBlackMapIntoMemory() asm (
+            "
+    ld de, BlackTilemap
+    ld hl, $9800
+    ld bc, WhiteTilemap - WhiteTilemapEnd
+    call CopyDEintoMemoryAtHL
+        "
+                );
     void
     void setTileMapBlack(Color color) asm(
     "
@@ -281,9 +292,9 @@ class GameBoy {
     /////////////////////////////////////////////////////////////////////////////////////
     // HARDWARE
     //////////////////////////////////////////////////////////////////////////////////////
-    void includeHardware() asm (
-        #include "hardware_inc"
-    );
+    //void includeHardware() asm (
+      //  #include "hardware_inc"
+    //);
     void endGame() asm (
         "
         Done:
--- src/main/deca/Utils.java
+++ src/main/deca/Utils.java
@@ -1,16 +1,100 @@
-class DoubleLinkedListEvents {
-    Event head = null;
-    Event tail = null;
 
-    void addEvent(Event event) {
-        if (head == null) {
-            head = event;
-            tail = event;
+class BackGroundMapMod {
+    protected boolean white = true;
+    protected boolean black = false;
+    protected boolean user = false;
+    protected boolean changed = false;
+
+    public void setWhite() {
+        this.changed = true;
+        this.white = true;
+        this.black = false;
+        this.user = false;
+    }
+
+    public void setBlack() {
+        this.changed = true;
+        this.white = false;
+        this.black = true;
+        this.user = false;
+    }
+
+    public void setUser() {
+        this.changed = true;
+        this.white = false;
+        this.black = false;
+        this.user = true;
+    }
+
+    public void setChanged(boolean changed) {
+        this.changed = changed;
+    }
+
+    boolean isWhite() {
+        return white;
+    }
+
+    boolean isBlack() {
+        return black;
+    }
+
+    boolean isUser() {
+        return user;
+    }
+    boolean hasChanged() {
+        return changed;
+    }
+}
+class DrawEvent {
+    protected int tileIndex;
+    protected int x;
+    protected int y;
+    protected int next = null;
+
+    int getTileIndex() {
+        return tileIndex;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    int getNext() {
+        return next;
+    }
+
+    void init(int index, int x, int y) {
+        this.tileIndex = index;
+        this.x = x;
+        this.y = y;
+    }
+    void setNext(DrawEvent event) {
+        this.next = event;
+    }
+    boolean hasNext() {
+        return this.next != null;
+    }
+}
+class DrawEventList() {
+    protected DrawEvent first = null;
+    protected DrawEvent last = null;
+    void add(int index, int x, int y) {
+        DrawEvent event = new DrawEvent();
+        event.init(index, x, y);
+        if (this.first == null) {
+            this.first = event;
+            this.last = event;
         } else {
-            tail.setNext(event);
-            event.setPrevious(tail);
-            tail = event;
+            this.last.setNext(event);
+            this.last = event;
         }
+    }
+    DrawEvent getFirst() {
+        return this.first;
     }
 }
 
@@ -32,14 +116,41 @@ class Utils {
             "
             ld bc, 49664
             ld [bc], bc
-            rts
             "
             ); // TODO VRAIMENT PAS SUR.
-    void push(int addr, int value) asm(
+    void pushInTileMap(int x, int y, int tileIndex) asm(
             "
-            ld bc, 49670
-            ld de, 49664
-            ld [bc], de
+            ; a = y
+            ld hl, sp + 7
+            ld a, [hl]
+
+            ; b = x
+            ld hl, sp + 6
+            ld b, [hl]
+
+            ; c = value
+            ld hl, sp + 8
+            ld c, [hl]
+
+            ; si y == 0 on passe à la boucle des colonnes
+            or a, a
+            jp z, yEqualsZero
+
+            ; hl c est la premiere adresse de la map
+            ld hl, $9800
+            ; de = 32
+            ld e, 32
+            ld d, 0
+            ; on incremente hl de y * 32
+            yLinesLoop :
+                add hl, de
+                dec a
+                jp nz, yLinesLoop
+
+            yEqualsZero :
+            ld e, b
+            add hl, de ;hl += x;
+            ld [hl], c
             "
             ); // TODO VRAIMENT PAS SÛR
 }
