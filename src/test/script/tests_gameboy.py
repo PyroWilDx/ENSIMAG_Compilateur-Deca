acc0c1d044f102a8873d9934d176f10d45dce960
Pierre <pierre.lin@grenoble-inp.org>
2024-01-22 23:56:13 +0100

    .

--- src/test/script/tests_gameboy.py
+++ src/test/script/tests_gameboy.py
@@ -115,10 +115,6 @@ def doTests():
     # doVerify("gameboy/object/hard/binaryTreeV1.deca")
     # doVerify("gameboy/object/hard/binaryTreeV2.deca")
 
-    # doVerify("gameboy/plot/init.deca")
-    # doVerify("gameboy/plot/point.deca")
-    # doVerify("gameboy/plot/ClasslessbouncingBallEasy.deca")
-
     return 0
 
 
