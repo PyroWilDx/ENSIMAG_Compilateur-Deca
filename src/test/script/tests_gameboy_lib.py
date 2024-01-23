ada5cea311b1cab311e44ad968b0305ce092aca2
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2024-01-23 22:43:28 +0100

    .

--- src/test/script/tests_gameboy_lib.py
+++ src/test/script/tests_gameboy_lib.py
@@ -68,7 +68,7 @@ def doTests():
     #doVerify("gameboy/plot/point.deca")
     #doVerify("gameboy/plot/ClasslessbouncingBallEasy.deca")
     #doVerify("gameboy/plot/bouncingBall.deca")
-    #doVerify("gameboy/plot/input_easy.java")
+#     doVerify("gameboy/plot/input_easy.java")
     doVerify("gameboy/plot/SnakeGame.deca")
 
 
